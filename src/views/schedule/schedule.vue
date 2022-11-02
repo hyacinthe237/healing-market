1303daea88eaf84f71c6f1cdee41111e2ec95fb3
Hyacinthe ABANDA <hyacinthabanda@gmail.com>
2022-11-02 16:30:08 +0100

    design schedule

--- src/views/schedule/schedule.vue
+++ src/views/schedule/schedule.vue
@@ -5,74 +5,51 @@
         <Sidebar :current="'schedule'" />
 
         <div id="page-content-wrapper">
-            <div class="dashboard">
-              <div class="cards">
-
-                <div class="card">
-                  <div class="gauche">
-                    <div class="number">45</div>
-                    <div class="name">Team members</div>
-                  </div>
-                  <div class="droite"><i class="feather icon-users"></i></div>
+            <div class="schedule">
+              <div class="header-box">
+                <div class="actions-gauche">
+                  <div class="gauche"><i class="ion-ios-skip-backward"></i></div>
+                  <div class="droite"><i class="ion-ios-skip-forward"></i></div>
+                  <div class="date">{{ date }}</div>
                 </div>
-
-                <div class="card">
-                  <div class="gauche">
-                    <div class="number">45</div>
-                    <div class="name">Clocked in</div>
-                  </div>
-                  <div class="droite"><i class="feather icon-user"></i></div>
+                <div class="select">
+                  <select name="date">
+                    <option>Week</option>
+                  </select>
+                  <select name="date">
+                    <option>Employee view</option>
+                  </select>
                 </div>
+                <div class="schedule-spacer"></div>
+                <div class="btns">
+                  <div class="action"><i class="ion-ios-settings"></i></div>
+                  <div class="action"><i class="ion-ios-print"></i></div>
+                  <div class="action"><i class="ion-md-cloud-download"></i></div>
+                </div>
+                <div class="filter" @click="displayFilter()"><i class="feather icon-filter"></i></div>
               </div>
 
-              <div>
-                  <div class="_tabs mt-20">
-                      <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                          <a class="nav-item nav-link" id="nav-upcoming-tab"
-                              data-toggle="tab" href="#nav-upcoming" role="tab"
-                              aria-controls="nav-upcoming">Now & Upcoming (1)</a>
-
-                          <a class="nav-item nav-link" id="nav-completed-tab"
-                              data-toggle="tab" href="#nav-completed" role="tab"
-                              aria-controls="nav-completed">Completed (0)</a>
-                      </div>
-                  </div>
-
-                  <div class="tab-content" id="nav-tabContent">
-                      <div class="tab-pane fade" id="nav-upcoming" role="tabpanel" aria-labelledby="nav-upcoming-tab">
-                          <div class="upcoming-box">
-                            <div class="upcoming-item">
-                              <div class="hour">5 pm</div>
-                              <div class="avatar">AH</div>
-                              <div class="content">
-                                <div class="name">
-                                  Abanda Hyacinthe
-                                  <div class="time">Starts in 1hr 32m / 4:45 pm - 9:45 am</div>
-                                </div>
-                                <div class="more"><i class="feather icon-more-vertical"></i></div>
-                              </div>
-                            </div>
-                          </div>
-                      </div>
-
-                      <div class="tab-pane fade" id="nav-completed" role="tabpanel" aria-labelledby="nav-completed-tab">
-                        <div class="upcoming-box">
-                          <div class="upcoming-item">
-                            <div class="hour">5 pm</div>
-                            <div class="avatar">AH</div>
-                            <div class="content">
-                              <div class="name">
-                                Abanda Hyacinthe
-                                <div class="time">Starts in 1hr 32m / 4:45 pm - 9:45 am</div>
-                              </div>
-                              <div class="more"><i class="feather icon-more-vertical"></i></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                  </div>
+              <div class="filter-box" v-if="showFilter">
+                <div class="actions-haut">
+                  <div class="gauche"><i class="ion-ios-skip-backward"></i></div>
+                  <div class="date">{{ date }}</div>
+                  <div class="droite"><i class="ion-ios-skip-forward"></i></div>
+                </div>
+                <div class="select">
+                  <select name="date">
+                    <option>Week</option>
+                  </select>
+                  <select name="date">
+                    <option>Employee view</option>
+                  </select>
+                </div>
+                <div class="schedule-spacer"></div>
+                <div class="btns">
+                  <div class="action"><i class="ion-ios-settings"></i></div>
+                  <div class="action"><i class="ion-ios-print"></i></div>
+                  <div class="action"><i class="ion-md-cloud-download"></i></div>
+                </div>
               </div>
-
             </div>
         </div>
     </div>
@@ -82,21 +59,31 @@
 <script>
 import Header from '@/components/commons/header/header'
 import Sidebar from '@/components/commons/sidebar/sidebar'
-import _ from 'lodash'
+import moment from 'moment'
 
 export default {
     data: () => ({
-        payload: {}
+        payload: {},
+        showFilter: false
     }),
 
     components: { Header, Sidebar },
 
-    computed: { },
+    computed: {
+      date () {
+        return 'March 26 - Apr 2, 2019'
+      }
+    },
 
     watch: { },
 
     mounted () { },
 
-    methods: { }
+    methods: {
+      displayFilter () {
+        this.showFilter = !this.showFilter
+        console.log('showfilter', this.showFilter)
+      }
+    }
 }
 </script>

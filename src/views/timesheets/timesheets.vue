edc476318fc2333d91c734a94560e472f57cf719
Hyacinthe ABANDA <hyacinthabanda@gmail.com>
2022-11-08 13:18:05 +0100

    timesheets

--- src/views/timesheets/timesheets.vue
+++ src/views/timesheets/timesheets.vue
@@ -5,75 +5,94 @@
         <Sidebar :current="'timesheets'" />
 
         <div id="page-content-wrapper">
-            <div class="dashboard">
-              <div class="cards">
-
-                <div class="card">
-                  <div class="gauche">
-                    <div class="number">45</div>
-                    <div class="name">Team members</div>
-                  </div>
-                  <div class="droite"><i class="feather icon-users"></i></div>
-                </div>
-
-                <div class="card">
-                  <div class="gauche">
-                    <div class="number">45</div>
-                    <div class="name">Clocked in</div>
-                  </div>
-                  <div class="droite"><i class="feather icon-user"></i></div>
+          <div class="timesheets">
+            <div class="header-box">
+              <div class="title">
+                <select>
+                  <option>Last 7 days: Fri Oct 28 - Fri Nov 4</option>
+                </select>
+                <div class="lg-btns">
+                  <div class="lg-btn-outline">View Totals</div>
                 </div>
               </div>
+              <div class="reste">
+                <div class="filter-table"><i class="ion-ios-tool"></i> filter table</div>
+                <div class="btns">
+                  <div class="action"><i class="ion-ios-print"></i></div>
+                  <div class="action"><i class="ion-md-cloud-download"></i></div>
+                </div>
 
-              <div>
-                  <div class="_tabs mt-20">
-                      <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                          <a class="nav-item nav-link" id="nav-upcoming-tab"
-                              data-toggle="tab" href="#nav-upcoming" role="tab"
-                              aria-controls="nav-upcoming">Now & Upcoming (1)</a>
-
-                          <a class="nav-item nav-link" id="nav-completed-tab"
-                              data-toggle="tab" href="#nav-completed" role="tab"
-                              aria-controls="nav-completed">Completed (0)</a>
-                      </div>
-                  </div>
-
-                  <div class="tab-content" id="nav-tabContent">
-                      <div class="tab-pane fade" id="nav-upcoming" role="tabpanel" aria-labelledby="nav-upcoming-tab">
-                          <div class="upcoming-box">
-                            <div class="upcoming-item">
-                              <div class="hour">5 pm</div>
-                              <div class="avatar">AH</div>
-                              <div class="content">
-                                <div class="name">
-                                  Abanda Hyacinthe
-                                  <div class="time">Starts in 1hr 32m / 4:45 pm - 9:45 am</div>
-                                </div>
-                                <div class="more"><i class="feather icon-more-vertical"></i></div>
-                              </div>
-                            </div>
-                          </div>
-                      </div>
-
-                      <div class="tab-pane fade" id="nav-completed" role="tabpanel" aria-labelledby="nav-completed-tab">
-                        <div class="upcoming-box">
-                          <div class="upcoming-item">
-                            <div class="hour">5 pm</div>
-                            <div class="avatar">AH</div>
-                            <div class="content">
-                              <div class="name">
-                                Abanda Hyacinthe
-                                <div class="time">Starts in 1hr 32m / 4:45 pm - 9:45 am</div>
-                              </div>
-                              <div class="more"><i class="feather icon-more-vertical"></i></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                  </div>
+                <div class="lg-btns">
+                  <div class="lg-btn-outline">Add Time Card</div>
+                  <div class="lg-btn-primary">Download</div>
+                </div>
               </div>
-
+              <div class="filter" @click="displayFilter()"><i class="feather icon-more-vertical"></i></div>
             </div>
+
+            <div class="filter-box" v-if="showFilter">
+              <div class="title">
+                <select name="last">
+                  <option value="">Last 7 days: Fri Oct 28 - Fri Nov 4</option>
+                </select>
+                <div class="lg-btns">
+                  <div class="lg-btn-outline">View Totals</div>
+                </div>
+              </div>
+              <div class="reste">
+                <div class="filter-table"><i class="feather icon-tool"></i> filter table</div>
+                <div class="btns">
+                  <div class="action"><i class="ion-ios-print"></i></div>
+                  <div class="action"><i class="ion-md-cloud-download"></i></div>
+                </div>
+
+                <div class="lg-btns">
+                  <div class="lg-btn-outline">Add Time Card</div>
+                  <div class="lg-btn-primary">Download</div>
+                </div>
+              </div>
+            </div>
+
+            <!-- List Box code -->
+            <div class="grey-box">
+              <div class="grey-head">
+                <i class="feather icon-chevron-down"></i>
+                <i class="feather icon-chevron-up"></i>
+                Filter Timesheets
+              </div>
+
+              <div class="inputs">
+                <div class="input">
+                  <div class="label">Filter By</div>
+                  <select>
+                    <option>All employees</option>
+                  </select>
+                </div>
+                <div class="input">
+                  <div class="label">Sort Employees By</div>
+                  <select>
+                    <option>First Name</option>
+                  </select>
+                </div>
+                <div class="input">
+                  <div class="label">Group By</div>
+                  <select>
+                    <option>Employee</option>
+                  </select>
+                </div>
+                <div class="input">
+                  <div class="label">Show/Hide Columns</div>
+                  <select>
+                    <option>12/22 Column</option>
+                  </select>
+                </div>
+                <div class="input-line">
+                  <input type="checkbox" checked>
+                  <h4>Hide Employees Without Hours</h4>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   </div>
@@ -82,20 +101,33 @@
 <script>
 import Header from '@/components/commons/header/header'
 import Sidebar from '@/components/commons/sidebar/sidebar'
+import moment from 'moment'
 
 export default {
     data: () => ({
-        payload: {}
+        payload: {},
+        showFilter: false
     }),
 
     components: { Header, Sidebar },
 
-    computed: { },
+    computed: {
+      last_seven_day () {
+        let today = moment().format('ddd MMM d')
+        let lastdat = moment().subtract(7, 'days').format('ddd MMM d')
+
+        return `${lastdat} - ${today}`
+      }
+    },
 
     watch: { },
 
-    mounted () { },
+    mounted () {
+      console.log(this.last_seven_day)
+    },
 
-    methods: { }
+    methods: {
+      displayFilter () { this.showFilter = !this.showFilter }
+    }
 }
 </script>

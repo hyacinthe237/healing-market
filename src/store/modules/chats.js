a1c676673a34b0b2363f0e31ba3817739ebf5099
Hyacinthe ABANDA <hyacinthabanda@gmail.com>
2023-05-08 17:47:10 +0100

    store chat

--- src/store/modules/chats.js
+++ src/store/modules/chats.js
@@ -6,7 +6,9 @@ export default {
 
     state: {
         chats: [],
-        chat: {}
+        contacts: [],
+        chat: {},
+        last_message: {},
     },
 
     mutations: {
@@ -14,19 +16,52 @@ export default {
             state.chat = payload
         },
 
+        SET_LAST_MESSAGE (state, payload) {
+            state.last_message = payload
+        },
+
         SET_CHATS (state, payload) {
             state.chats = payload
-        }
-    },
+        },
 
-    actions: {
-        async getFile ({ commit }, id) {
-            const response = await ApiService.get(`/api/organisation/folder/${id}/update`)
-                .catch(error => console.log(error.response.data.errors))
-
-            commit('SET_FILE', response.data)
+        SET_CONTACTS (state, payload) {
+            state.contacts = payload
         },
     },
 
-    getters: {}
+    actions: {
+        async getChat ({ commit }, id) {
+            const response = await ApiService.get(`/chat-api/${id}`)
+                .catch(error => console.log(error.response.data.errors))
+
+            commit('SET_CHAT', response.data)
+        },
+        async getChats ({ commit }) {
+            const response = await ApiService.get(`/chat-api/`)
+                .catch(error => console.log(error.response.data.errors))
+
+            commit('SET_CHATS', response.data)
+        },
+
+        async getLastMessage ({ commit }) {
+            const response = await ApiService.get(`/chat-api/last-message/`)
+                .catch(error => console.log(error.response.data.errors))
+
+            commit('SET_LAST_MESSAGE', response.data)
+        },
+
+        async getContacts ({ commit }) {
+            const response = await ApiService.get(`/chat-api/contacts/`)
+                .catch(error => console.log(error.response.data.errors))
+
+            commit('SET_CONTACTS', response.data)
+        },
+    },
+
+    getters: {
+        getChat: state => state.chat,
+        getChats: state => state.chats,
+        getContacts: state => state.contacts,
+        getLastMessage: state => state.last_message,
+    }
 }
